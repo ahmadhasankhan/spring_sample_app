734232acad6ec6aafae8419b7dcd453f69f6cd6f
Ahmad Hassan <er.ahmad.hassan@gmail.com>
2017-06-07 02:06:36 +0530

    autowire byName

--- src/main/java/com/learning/service/CustomerServiceImpl.java
+++ src/main/java/com/learning/service/CustomerServiceImpl.java
@@ -25,7 +25,7 @@ public class CustomerServiceImpl implements CustomerService {
         return customerRepository.findAll();
     }
 
-    public void setCustomerRepository(CustomerRepository customerRepository) {
+    public void setFoo(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
